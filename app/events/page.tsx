aaf0abf7179c18ba1efc0765bff5aa255f53f442
JavierGonzalez998 <javiergonzaleze98@gmail.com>
2024-10-17 19:14:41 -0300

    first commit

---
+++ app/events/page.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+export default function Events():React.JSX.Element{
+    return(
+    <main className="container mx-auto px-4 w-screen h-screen">
+        <div className="flex flex-row justify-center items-center w-full">
+            <h1>Eventos disponibles</h1>
+        </div>
+        
+    </main>
+    )
+}
